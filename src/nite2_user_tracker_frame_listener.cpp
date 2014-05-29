2e8313bfeb6fe794cd07fc70747677287b0266fa
Jordi Pages <jordi.pages@pal-robotics.com>
2014-05-29 13:11:27 +0200

    add nite::UserTracker successfully
    two topic added:
    
    /camera/num_users: number of users being tracked
    /camera/user_map: image showing segmented users each one painted in a different color

---
+++ src/nite2_user_tracker_frame_listener.cpp
@@ -0,0 +1,40 @@
+#include "openni2_camera/nite2_user_tracker_frame_listener.h"
+
+#include "NiTE-2/NiTE.h"
+
+#include <ros/ros.h>
+
+namespace nite2_wrapper
+{
+
+NiTE2UserTrackerFrameListener::NiTE2UserTrackerFrameListener()
+{
+}
+
+NiTE2UserTrackerFrameListener::~NiTE2UserTrackerFrameListener()
+{
+}
+
+void NiTE2UserTrackerFrameListener::onNewFrame(nite::UserTracker& userTracker)
+{
+  if ( userTracker.isValid() )
+  {
+    nite::Status niteRc;    
+    niteRc = userTracker.readFrame(&userTrackerFrame_);
+    if (niteRc != nite::STATUS_OK)
+    {
+      ROS_ERROR("nite::UserTracker: get next frame failed");
+    }
+    else
+    {
+      if ( userTrackerFrame_.isValid() )
+      {
+        callback_(userTrackerFrame_);
+      }
+      else
+        ROS_WARN("Skipping user tracker frame as it is not valid");
+    }
+  }
+}
+
+}

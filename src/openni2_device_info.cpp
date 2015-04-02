8c400fa8681b898db9bb3b3d40f1e6ad9eb87215
Stephan Wirth <wirth@savioke.com>
2015-04-02 03:28:28 -0700

    print vendor id and product id as hex value (like in lsusb)

--- src/openni2_device_info.cpp
+++ src/openni2_device_info.cpp
@@ -38,8 +38,8 @@ namespace openni2_wrapper
 std::ostream& operator << (std::ostream& stream, const OpenNI2DeviceInfo& device_info) {
   stream << "Uri: " << device_info.uri_ << " (Vendor: " << device_info.vendor_ <<
                                            ", Name: " << device_info.name_ <<
-                                           ", Vendor ID: " << device_info.vendor_id_ <<
-                                           ", Product ID: " << device_info.product_id_ <<
+                                           ", Vendor ID: " << std::hex << device_info.vendor_id_ <<
+                                           ", Product ID: " << std::hex << device_info.product_id_ <<
                                              ")" << std::endl;
   return stream;
 }
